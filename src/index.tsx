3b070f0ab401567a34a09d4c514763909f1b11a0
emtsn <65764699+emtsn@users.noreply.github.com>
2023-05-20 00:09:33 -0700

    Minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { Gamemode } from './models/gamemode';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App gamemode={Gamemode.Random} />
+        <App />
     </React.StrictMode>
 );
 
